8e732e3b67c282e0300c0af80181fe33e93680a4
gothaw <rad.soltan@gmail.com>
2018-10-01 23:03:56 +0100

    adding functionality for angle options button

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,28 @@
     const advanceOptions = document.getElementsByClassName("advance-options")[0];
     const triHypFunctionBtn = document.getElementById("tri-hyp");
     const triHypFunctions = document.getElementsByClassName("tri-hyp-function");
+    const angleOptionBtn = document.getElementById("deg-rad-gra");
 
     //true is second tab of advance options is shown
     let secondTab   = false;
 
+    /**
+     * @name    advanceOptions
+     * @desc    toggles between degrees, radians and grads
+     */
+    function angleOptions() {
+        switch (angleOptionBtn.innerHTML) {
+            case "deg":
+                angleOptionBtn.innerHTML="rad";
+                break;
+            case "rad":
+                angleOptionBtn.innerHTML="gra";
+                break;
+            default:
+                angleOptionBtn.innerHTML="deg";
+        }
+    }
+    
     /**
      * @name    triHypFunctions
      * @desc    toggles between trigonometric and hyperbolic options for advance functions, modifies inner HTML for these functions
@@ -46,6 +64,7 @@
     function eventHandler() {
         toggleBtn.addEventListener("click",showOptions);
         triHypFunctionBtn.addEventListener("click",triHypOptions);
+        angleOptionBtn.addEventListener("click",angleOptions);
     }
 
     function init() {
